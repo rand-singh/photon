1e555968a1c466f42a64c0b38b7e0c4eeda56abf
Randeep Singh <randeeprattan90@gmail.com>
2020-05-21 16:21:06 +0100

    Implement search feature

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ import { config } from "./config.js";
 const authKey = config.AUTH,
   gallery = document.querySelector(".gallery"),
   searchInput = document.querySelector(".search-input"),
-  submitButton = document.querySelector(".submit-button");
+  form = document.querySelector(".search-form");
 let searchValue;
+
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
 
 async function curatedPhotos() {
   const dataFetch = await fetch(
@@ -19,7 +29,32 @@ async function curatedPhotos() {
   );
 
   const data = await dataFetch.json();
-  console.log(data);
+
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `
+        <img src="${photo.src.large}"/> 
+        <p>${photo.photographer}</p>
+    `;
+
+    gallery.appendChild(galleryImg);
+  });
+}
+
+async function searchPhotos(query) {
+  const dataFetch = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}&per_page=10`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: authKey,
+      },
+    }
+  );
+  const data = await dataFetch.json();
+
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
